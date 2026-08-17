956dc07b0ff320340c81dc1c44a57861aba324ff
Xyonox <93388182+xyonox@users.noreply.github.com>
2026-08-17 13:47:15 +0200

    Setup

--- main.go
+++ main.go
@@ -1,5 +1,31 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"os"
+)
 
+// For env variables
+// go get github.com/joho/godotenv
+
+func loadSQL() {
+
+}
+
+func run() error {
+
+	// Password for database scanner input
+	// if frist time run, set password
+
+	return nil
+}
+
+func main() {
+	// First steps
+	// Load sqlite database
+	// Create tables
+	if err := run(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 }
